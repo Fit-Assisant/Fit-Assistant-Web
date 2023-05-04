42ff291995e44f884c6e0a1b4e9125c76669a917
Maxence <maxence.boucquez@etu.univ-lyon1.fr>
2023-05-04 22:45:03 +0200

    fix short navbar + beginning home page

--- fit-assistant/src/pages/Home/home.tsx
+++ fit-assistant/src/pages/Home/home.tsx
@@ -1,11 +1,18 @@
+import HomeImage from "./HomeImage/HomeImage";
 import "./home.css";
 
 function Home() {
   return (
     <div className={"home"}>
       <h1>Home</h1>
-      <img src="../assets/account.png" alt="account" />
+      <HomeImage/>
+      <div className="text-wrapper">
+      <p> Welcome on Fit Assistant AKA your best workout tracker friend.</p>
+      <p>On this website, you'll have access to all our workout exercises and programs. We are here to help you not to be lost in your gym the first time.</p>
+      <p>Not only for the beginners, you will have the possibilities to save all your PR (Personnal Records) to see your progression curve.</p>
     </div>
+    </div>
+    
   );
 }
 
